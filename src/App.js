5b9511e1a451f4e74e9b81246e9b4dbb06de0464
Abhisheknayak-programer <sahilservice300@gmail.com>
2021-03-29 08:45:05 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
 import React from "react";
+import { Redirect, Route, Switch } from "react-router";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+    return (
+        <>
+            <Navbar/>
+            <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/contact" component={Contact}/>
+            <Redirect to="/"/>
+            </Switch>
+        </>
+    )
+}
+
+export default App;
